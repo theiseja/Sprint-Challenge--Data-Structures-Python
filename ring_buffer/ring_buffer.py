795c8c218b7d34e900276d0e5655cc0d9f59684e
Jesse Theisen <theiseja@gmail.com>
2020-02-22 11:25:27 -0600

    Completes Ring Buffer with passing tests aside from Stretch Goal

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,17 +8,39 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        # TBC
-        self.storage[self.current] = item 
-        if self.current == (self.capacity -1):
-                self.current = 0
-        else:
-            self.current += 1
+        # if storage isn't full
+        if self.storage.length < self.capacity:
+            #adds to tail
+            self.storage.add_to_tail(item)
+            #updates current
+            self.current = self.storage.head
+            #if storage is full
+        elif self.storage.length == self.capacity:
+            #remove head since it is oldest value to free space
+            remove_head = self.storage.head
+            self.storage.remove_from_head()
+            #adds item to tail and becomes newest value
+            self.storage.add_to_tail(item)
+            #if head set current pos to tail
+            if remove_head == self.current:
+                self.current = self.storage.tail
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
-
+        start = self.current
+        list_buffer_contents.append(start.value)
+        #loop through nodes to append value
+        if start.next:
+            nxt = start.next #next
+        else:
+            nxt = self.storage.head
+        while nxt != start:
+            list_buffer_contents.append(nxt.value)
+            if nxt.next:
+                nxt = nxt.next
+            else:
+                nxt = self.storage.head
         # TODO: Your code here
 
         return list_buffer_contents
